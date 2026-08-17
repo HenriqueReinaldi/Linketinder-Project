d80fd31a073c0bca227bbdf598874dd0cf4fb144
HenriqueReinaldi <henrique.f.reinaldi@gmail.com>
2026-08-17 14:13:34 -0300

    feat: funcoes cadastro terminal + testes

--- src/main/groovy/org/linketinder/terminal/Terminal.groovy
+++ src/main/groovy/org/linketinder/terminal/Terminal.groovy
@@ -1,19 +1,75 @@
 package org.linketinder.terminal
 
 import groovy.transform.TupleConstructor
+import org.linketinder.classes.Candidato
+import org.linketinder.classes.Empresa
+import org.linketinder.dados.Cadastro
 import org.linketinder.dados.Dados
 
 @TupleConstructor
 class Terminal {
     Dados dados
+    Cadastro cadastro
+
+    Candidato cadastrar_candidato(){
+        Scanner scan = new Scanner(System.in);
+
+        try{
+            String nome = scan.nextLine();
+            String email = scan.nextLine();
+            String estado  = scan.nextLine();
+            String CEP = scan.nextLine();
+            String descricao = scan.nextLine();
+            String CPF = scan.nextLine();
+            int idade = Integer.parseInt(scan.nextLine());
+            List<String> competencias  = scan.nextLine().split(" ");
+
+            Candidato c = new Candidato(nome, email, estado, CEP, descricao, CPF, idade, competencias)
+
+            if (cadastro.cadastrar_candidato(c)){
+                return c
+            }
+            return null
+        }
+        catch (Exception ignored){
+            return null;
+        }
+    }
+
+    Empresa cadastrar_empresa(){
+        Scanner scan = new Scanner(System.in);
+
+        try{
+            String nome = scan.nextLine();
+            String email = scan.nextLine();
+            String estado  = scan.nextLine();
+            String CEP = scan.nextLine();
+            String descricao = scan.nextLine();
+            String CNPJ = scan.nextLine();
+            String pais = scan.nextLine();
+            List<String> competencias  = scan.nextLine().split(" ");
+
+            Empresa e = new Empresa(nome, email, estado, CEP, descricao, CNPJ, pais, competencias)
+
+            if (cadastro.cadastrar_empresa(e)){
+                return e
+            }
+            return null
+        }
+        catch (Exception ignored){
+            return null;
+        }
+    }
+
 
     int receber_input(String input){
-
         switch (input){
             case "?":
-                println "listar candidatos | mostra todos os candidatos"
-                println "listar empresas   | mostra todos as empresas"
-                println "sair              | fecha o programa"
+                println "listar candidatos    | mostra todos os candidatos"
+                println "listar empresas      | mostra todos as empresas"
+                println "cadastrar candidato  | "
+                println "cadastrar empresa    | "
+                println "sair                 | fecha o programa"
                 return 0
             case "listar candidatos":
                 dados.candidatos.each {it.representacao()}
@@ -24,7 +80,6 @@ class Terminal {
             case "sair":
                 return 1
         }
-
 
         0
     }
